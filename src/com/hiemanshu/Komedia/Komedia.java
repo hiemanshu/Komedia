8e158941bf7c464d5d5814f492bd4f1ad7df31c1
Hiemanshu Sharma <hiemanshu@gmail.com>
2013-03-04 21:26:10 +1100

    Intial Commit

---
+++ src/com/hiemanshu/Komedia/Komedia.java
@@ -0,0 +1,15 @@
+package com.hiemanshu.Komedia;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Komedia extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
